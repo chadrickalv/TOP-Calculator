9f0b2e9b14afab6a8eecc8bf37118915ce1e0f5b
Chadrick <chadrickalvarez@gmail.com>
2025-12-17 17:31:56 -0500

    Able to display numbers. Removed CSS layout, needs improvement.

--- script.js
+++ script.js
@@ -14,10 +14,28 @@ function divide (a, b) {
     return b===0 ? 'ERR, Not divisible by 0' : a/b
 }
 
-function operate(op, a, b){
+/*function operate(op, a, b){
 
 }
-
+*/
 console.log(add(2,3))
 console.log(multiply(3,0))
 console.log(divide(3,0))
+
+//functions to populate the display with the digit numbers on click
+const display = document.querySelector('.display')
+const numsButton = document.querySelectorAll('.nums button');
+numsButton.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent += button.textContent;
+    })
+});
+
+/* 
+finished displaying. 
+missing: 
+-   storing the content of the display in a 
+    variable for use in the next step
+
+-   brand new css layout (old one sucked)
+*/
