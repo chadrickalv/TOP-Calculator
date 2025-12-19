ed8f79b3dc8fca6a4100803dc1097df29aae0db0
Chadrick <chadrickalvarez@gmail.com>
2025-12-19 02:55:22 -0500

    created the logic for the clear button. created bones for operate function.

--- script.js
+++ script.js
@@ -14,33 +14,54 @@ function divide (a, b) {
     return b===0 ? 'ERR, Not divisible by 0' : a/b
 }
 
-/*function operate(op, a, b){
 
-}
-*/
 console.log(add(2,3))
 console.log(multiply(3,0))
 console.log(divide(3,0))
+
+//function that will be called when the user clicks on '='
+function operate (a, b, op) {
+
+}
 
 let currentInput = '';
 
 //functions to populate the display with the digit numbers on click
 const display = document.querySelector('.display')
-const numsButton = document.querySelectorAll('.nums button');
+const numsButton = document.querySelectorAll('.nums button:not(.equals)');
 numsButton.forEach(button => {
     button.addEventListener('click', () => {
-        currentInput += button.textContent;
+        if (currentInput === '' || currentInput === '0'){
+            currentInput = button.textContent //replace the 0
+        } else {
+            currentInput += button.textContent; //append numbers normally
+        }
         display.textContent = currentInput;
         display.scrollLeft = display.scrollWidth
     })
 });
 
 
-/* 
-finished displaying. 
-missing: 
--   storing the content of the display in a 
-    variable for use in the next step
 
--   brand new css layout (old one sucked)
+
+
+//clear button logic
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', () => {
+    firstNumber = null;
+    operators = null;
+    currentInput = '';
+    result = null;
+    display.textContent = '0';
+});
+
+
+
+
+/* 
+added variable content holder
+missing: 
+-   operator functionality
+
+
 */
