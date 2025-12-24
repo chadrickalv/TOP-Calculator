142f854ccfd65615b96cce1cc137f06fd60c49cf
Chadrick <chadrickalvarez@gmail.com>
2025-12-23 22:57:48 -0500

    A working Calculator using V1

--- script.js
+++ script.js
@@ -21,10 +21,23 @@ console.log(divide(3,0))
 
 //function that will be called when the user clicks on '='
 function operate (a, b, op) {
-
+    switch (op){
+        case '+': 
+           return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return b===0 ? 'ERR, Not divisible by 0' : a/b    
+    }
 }
 
+
 let currentInput = '';
+let firstNumber = null;
+let operator = null;
+let result = null;
 
 //functions to populate the display with the digit numbers on click
 const display = document.querySelector('.display')
@@ -42,6 +55,36 @@ numsButton.forEach(button => {
 });
 
 
+const symbolsButton = document.querySelectorAll('.symbols button')
+symbolsButton.forEach(symbolsbtn => {
+    symbolsbtn.addEventListener('click', () => {
+        if (firstNumber == null){
+        firstNumber = parseFloat(currentInput)
+        operator = symbolsbtn.textContent
+        currentInput = ''
+        } else if (firstNumber !== null){
+            secondNumber = parseFloat(currentInput)
+            result = operate(firstNumber, secondNumber, operator)
+            firstNumber = result
+            operator = symbolsbtn.textContent
+            currentInput = ''
+            display.textContent = firstNumber
+        }
+    })
+})
+
+
+
+const equalsButton = document.querySelector('.equals')
+equalsButton.addEventListener('click', () => {
+
+        secondNumber = parseFloat(currentInput)
+        result = operate(firstNumber, secondNumber, operator)
+        currentInput = result;
+        display.textContent = result;
+        display.scrollLeft = display.scrollWidth
+})
+
 
 
 
@@ -49,19 +92,82 @@ numsButton.forEach(button => {
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
     firstNumber = null;
-    operators = null;
+    operator = null;
     currentInput = '';
     result = null;
     display.textContent = '0';
 });
 
 
+/* logic to make the display into string 
+inputs of numbers which will then all be combined when 
+'=' is pressed (input: 1+1+1+1; press -> '='; output: 4;)
 
+const symbolsButton = document.querySelectorAll('.symbols button')
+symbolsButton.forEach(symbolsbtn => {
+    symbolsbtn.addEventListener('click', () => {
+        if (firstNumber == null){
+        firstNumber = currentInput
+        operator = symbolsbtn.textContent
+        currentInput = ''
+        display.textContent += symbolsbtn.textContent
+        } else if (firstNumber !== null){
+            secondNumber = currentInput
+            operator = symbolsbtn.textContent
+            currentInput = ''
+            display.textContent += symbolsbtn.textContent
+        } 
+    })
+})
+
+*/
 
 /* 
 added variable content holder
 missing: 
 -   operator functionality
 
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
 
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+
+const browser = ''
+if (browser === 'Edge'){
+    console.log("You've got the Edge!");
+} else if (browser == 'Chrome'
+ || browser == 'Firefox'
+ || browser == 'Safari'
+ || browser == 'Opera'){
+     console.log('Okay we support these browsers too.')//you can also use 'alert' instead of console.log
+} else {
+    console.log('We hope that this page looks ok!')
+}
+
+
+let a = +prompt('a?', '');
+
+switch(a) {
+case 0:
+    alert( 0 );
+    break;
+case 1:
+    alert ( 1 );
+    break;
+case 2:
+case 3:
+    alert ( '2,3' );
+    break;
+}
 */
